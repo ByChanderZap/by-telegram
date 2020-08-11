65afb76802727a643500c190c12b6aab69f10ffa
Alexander Herrera <chander.zap@gmail.com>
2020-08-10 22:41:42 -0500

    [ADD] Connection to mongodb

--- components/messages/storage.js
+++ components/messages/storage.js
@@ -1,4 +1,13 @@
-const list = [];
+const db = require('mongoose');
+const { dbURL } = require('./dbkeys');
+
+db.Promise = global.Promise;
+db.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+console.log('[DB] Conecction success')
 
 const addMessage = (message) => {
     list.push(message)
